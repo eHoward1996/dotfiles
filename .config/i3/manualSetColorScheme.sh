df322331cd9ee3e77142a5168fc3694b9b2b523c
Evan Howard <evan.howard96@gmail.com>
2019-01-05 07:02:16 -0500

    Small update to manual color scheme setter script

--- .config/i3/manualSetColorScheme.sh
+++ .config/i3/manualSetColorScheme.sh
@@ -1,14 +1,18 @@
 if [[ ! -d "${HOME}/Pictures/.temp" ]]; then
     mkdir $HOME/Pictures/.temp
+fi;
 
-    for f in $HOME/Pictures/Wallpapers/*; do
-        fname=${f##*/}
-        fname=$(echo ${fname} | cut -d '.' -f 1)
-        str=$"${HOME}/Pictures/.temp/${fname}.png"
+for f in $HOME/Pictures/Wallpapers/*; do
+    fname=${f##*/}
+    fname=$(echo ${fname} | cut -d '.' -f 1)
+    str=$"${HOME}/Pictures/.temp/${fname}.png"
+
+    if [[ ! -f $str ]]; then
         echo 'created' $str 'from' $f
         convert $f $str
-    done;
-fi;
+    fi;
+done;
+
 newTheme=$(python /home/oonebaddog/Programs/dotfiles/.config/i3/imagePreview.py)
 newTheme=${newTheme##*/}
 newTheme=$(echo ${newTheme} | cut -d '.' -f 1)
