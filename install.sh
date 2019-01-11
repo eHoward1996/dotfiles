dc5ae25a15422992ddd6a8ff8ff7207bbf6df34f
Evan Howard <evan.howard96@gmail.com>
2019-01-11 00:11:45 -0500

    Rearrange install order

--- install.sh
+++ install.sh
@@ -2,14 +2,11 @@
 echo -e "Build basic packages...."
 read -p "Continue? (Y/N): " confirm
 
-if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+if [[ $confirm != [nN] ]]; then
     sudo pacman -S \
         arandr \
-        base \
-        base-devel \
         bash-completion \
         curl \
-        git \
         go \
         i3blocks \
         i3-gaps \
@@ -27,11 +24,23 @@ if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
         zsh
 fi
 
+# Move dotfiles
+echo -e "Moving Dotfiles...."
+read -p "Continue? (Y/N): " confirm
+
+if [[ $confirm != [nN] ]]; then
+    cp .config/i3 ~/.config
+    cp .config/polybar ~/.config
+    cp individual_dots/.zprofile ~/.config/zsh/.zprofile
+    cp individual_dots/.zshrc ~/.config/zsh/.zshrc
+    sudo cp individual_dots/etc.issue /etc/issue
+fi
+
 # Use Trizen to install AUR packages.
 echo -e "Build Trizen for AUR support...."
 read -p "Continue? (Y/N): " confirm
 
-if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+if [[ $confirm != [nN] ]]; then
     git clone https://aur.archlinux.org/trizen.git
     cd trizen
     makepkg -si
@@ -48,21 +57,9 @@ fi
 echo -e "Build Antigen for added ZSH support...."
 read -p "Continue? (Y/N): " confirm
 
-if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+if [[ $confirm != [nN] ]]; then
     mkdir ~/.config/antigen
     curl -L git.io/antigen > ~/.config/antigen/antigen.zsh
-fi
-
-# Move dotfiles
-echo -e "Moving Dotfiles...."
-read -p "Continue? (Y/N): " confirm
-
-if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
-    cp .config/i3 ~/.config
-    cp .config/polybar ~/.config
-    cp individual_dots/.zprofile ~/.zprofile
-    cp individual_dots/.zshrc ~/.zshrc
-    sudo cp individual_dots/etc.issue /etc/issue
 fi
 
 # Add programming language support? (Requires user input)
@@ -71,6 +68,6 @@ echo -e "Languages to be installed:\n"
 echo -e "\t-Python 3\n\t-Ruby\n\t-Java (JDK)\n"
 read -p "Continue? (Y/N): " confirm
 
-if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+if [[ $confirm != [nN] ]]; then
     trin jdk python ruby
 fi
