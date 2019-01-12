de0f369b6ac085d614e1fb383290559ce4d4676a
Evan Howard <evan.howard96@gmail.com>
2019-01-12 17:43:19 -0500

    Add .Xresources

--- install.sh
+++ install.sh
@@ -38,6 +38,7 @@ if [[ $confirm != [nN] ]]; then
     cp -r dotfiles/.config/polybar ~/.config
     cp dotfiles/individual_dots/.zprofile ~/.config/zsh/.zprofile
     cp dotfiles/individual_dots/.zshrc ~/.config/zsh/.zshrc
+    cp dotfiles/individual_dots/.Xresources ~/.Xresources
     sudo cp dotfiles/individual_dots/etc.issue /etc/issue
 fi
 
